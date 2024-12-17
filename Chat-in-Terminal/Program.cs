5af3fd7cb2c734b935d36971de219603af914fda
TaiStudent <xTaig4@gmail.com>
2024-12-17 14:38:17 +0100

    publisher and consumer added
    make sure each client is subscribed to channel

--- Chat-in-Terminal/Program.cs
+++ Chat-in-Terminal/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        //await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        bool isInChat = true;
+        while (isInChat) {
+
+            //Publisher
+            string message = Console.ReadLine().ToString();
+            var body = Encoding.UTF8.GetBytes(message);
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+
+            //Consumer
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received: {message}");
+                return Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+        }
+    }
+
+
+}
